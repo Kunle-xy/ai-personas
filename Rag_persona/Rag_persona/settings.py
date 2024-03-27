ef05a759ee66f4be787157ef9f07d44e43036492
Kunle-xy <oguntoye@iastate.edu>
2024-03-27 12:25:44 -0500

    more

--- Rag_persona/Rag_persona/settings.py
+++ Rag_persona/Rag_persona/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
